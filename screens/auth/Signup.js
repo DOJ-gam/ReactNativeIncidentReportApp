642e89df957b7c1f5d48c4d74a50313d7a96d50b
DOJ <omarjeng999@gmail.com>
2022-02-06 06:09:23 +0000

    signup ui

--- screens/auth/Signup.js
+++ screens/auth/Signup.js
@@ -1,19 +1,69 @@
-import * as React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import Button from '../../components/form/Button';
+import FormContainer from '../../components/form/FormContainer';
 import Input from '../../components/form/Input';
 // import {useNavigation} from '@react-navigation/native';
+const BgImage = require('../../assets/images/dojbg.png');
 
 const Signup = ({navigation}) => {
   // const navigation = useNavigation();
+  const [title, setTitle] = useState('SignUp!');
+
   return (
-    <View>
-      <Text>Signup</Text>
-      <Button title="Sigin" onPress={() => navigation.navigate('Login')} />
-      <Input placeholder="hii" />
+    <View style={styles.container}>
+      <FormContainer title={title} BgImage={BgImage}>
+        <Text style={{fontSize: 15, color: '#fff'}}>
+          Looks like you dont have an account.
+        </Text>
+        <Text style={{fontSize: 15, color: '#fff'}}>
+          Lets Create a new account for
+        </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            color: '#fff',
+            fontWeight: 'bold',
+            marginBottom: 10,
+          }}>
+          urname@gmail.com
+        </Text>
+        <Input placeholder="Name" />
+        <Input placeholder="Password" />
+
+        <Text style={{color: 'white'}}>
+          By selecting Agree and continue below,
+        </Text>
+        <View
+          style={{
+            alignItems: 'center',
+            flexDirection: 'row',
+            marginBottom: 15,
+          }}>
+          <Text style={{color: 'white', marginRight: 4}}>I agree to</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
+            <Text style={{fontSize: 17, color: 'tomato'}}>
+              Terms of Service and Privacy Policy
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Button text={'Agree and continue'} bg={'tomato'} color={'#fff'} />
+      </FormContainer>
     </View>
   );
 };
 
 export default Signup;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
